e91e6cfd6d0a7dbbc7399acc823cb48d725582b1
David Cermeño Moranchel <davidcermenomoranchel@MacBook-Pro-de-David-2.local>
2016-03-05 16:32:43 -0600

    add venv ignore

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/davidcermenomoranchel/Development/Dev.f-negra/MicroBuzz/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

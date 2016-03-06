4508db81fc936bf9e1c58d40a97c1f423a9b712c
Uriel Miranda <urielnovoa@gmail.com>
2016-03-06 04:17:53 -0600

    bla

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,30 @@
 (function(){
   'use strict';
-  angular.module("microbuzz",['ngResource','ngRoute']);
+  angular.module("microbuzz",['ngResource'])
+  .factory("apiMapaton",apiMapaton)
+  .directive("mapaBuzz",mapaBuzz);
 
+  apiMapaton.$inject = ["$resource"];
+  function apiMapaton($resource){
+    r = $resource('./server/mapaton.json',{},{});
+    console.log(r);
+
+  }
+
+  function mapaBuzz(){
+    var directive = {
+      restrict: 'EA',
+          templateUrl: '/templates/ctrl_home.html',
+          controller: mapatonController,
+          controllerAs: 'mapa',
+          replace : true
+        };
+        return directive;
+    }
+
+    function mapatonController(apiMapaton){
+      var mapa = this;
+      mapa.name = "hola";
+
+    }
 })();
---
+++ static/js/routes/routes.js
@@ -0,0 +1,6 @@
+(function(){
+  'use strict';
+
+  angular.module("microbuzz")
+  .config(configuration);
+})();

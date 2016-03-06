0ad63eb0f3195a1c85175159097875a60cf7749a
David Cermeño Moranchel <davidcermenomoranchel@MacBook-Pro-de-David-2.local>
2016-03-06 11:10:54 -0600

    add url index.html

--- MicroBuzz/urls.py
+++ MicroBuzz/urls.py
@@ -17,13 +17,15 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from login import views as viewsChofer
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('', include('social.apps.django_app.urls', namespace='social')),
     # Home URL
-
+    #Index
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name="index"),
 	# Logout URL
 	url(r'^users/logout/$','django.contrib.auth.views.logout',{'next_page': '/'},name="user-logout"),
     url(r'^chofer/$', viewsChofer.base, name="chofer"),

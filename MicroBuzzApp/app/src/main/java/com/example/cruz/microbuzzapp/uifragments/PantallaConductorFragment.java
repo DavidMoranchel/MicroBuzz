04a448597dd29e2d9033570ca3269b862807f89c
Cruz Cast <abstractime@outlook.com>
2016-03-06 13:28:49 -0600

    Pantalla usuario completa

--- MicroBuzzApp/app/src/main/java/com/example/cruz/microbuzzapp/uifragments/PantallaConductorFragment.java
+++ MicroBuzzApp/app/src/main/java/com/example/cruz/microbuzzapp/uifragments/PantallaConductorFragment.java
@@ -1,8 +1,13 @@
 package com.example.cruz.microbuzzapp.uifragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.cruz.microbuzzapp.R;
 
@@ -10,6 +15,19 @@ import com.example.cruz.microbuzzapp.R;
  * Created by Cruz on 05/03/2016.
  */
 public class PantallaConductorFragment extends Fragment {
+    private View root;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        root = inflater.inflate(R.layout.fragment_pantalla_conductor,container,false);
+        return root;
+    }
+
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         //super.onCreateOptionsMenu(menu, inflater);
--- MicroBuzzApp/app/src/main/java/com/example/cruz/microbuzzapp/uifragments/PantallaPeatonFragment.java
+++ MicroBuzzApp/app/src/main/java/com/example/cruz/microbuzzapp/uifragments/PantallaPeatonFragment.java
@@ -1,8 +1,13 @@
 package com.example.cruz.microbuzzapp.uifragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.cruz.microbuzzapp.R;
 
@@ -10,9 +15,20 @@ import com.example.cruz.microbuzzapp.R;
  * Created by Cruz on 05/03/2016.
  */
 public class PantallaPeatonFragment extends Fragment {
+
+    private View root;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        root = inflater.inflate(R.layout.fragment_pantalla_usuario,container,false);
+        return root;
+    }
+
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         //super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.menu_peaton,menu);
     }
+
 }
